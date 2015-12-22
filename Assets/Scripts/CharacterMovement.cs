da9a647446c2b702048e9933bcf592e2d1ae0344
Anisuzzaman Khan <aniskhan001@outlook.com>
2015-12-22 23:37:28 +0600

    player can now face left and right while moving

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -12,13 +12,22 @@ public class CharacterMovement : MonoBehaviour {
     // Use this for initialization
     void FixedUpdate () {
         GetComponent<Rigidbody>().velocity = new Vector2(moveDirectionX * maxSpeed, moveDirectionY * maxSpeed);
-        //gravitySpeed = Physics.gravity.y * 1.5;
-        GetComponent<Rigidbody>().AddForce(3 * Physics.gravity);
+
+        if (moveDirectionX < 0.0f && facingRight) // player tries to go to the left (-ve) and facing right
+            Flip();
+        else if (moveDirectionX > 0.0f && !facingRight) // player tries to go to the right (+ve) and facing left
+            Flip();
     }
 	
 	// Update is called once per frame
 	void Update () {
         moveDirectionY = Input.GetAxis("Vertical");
         moveDirectionX = Input.GetAxis("Horizontal");
+    }
+
+    void Flip () {
+        facingRight = !facingRight;
+
+        transform.Rotate(Vector3.up, 180.0f, Space.World);
     }
 }
